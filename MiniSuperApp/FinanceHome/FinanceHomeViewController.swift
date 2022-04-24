8cfb4c2d5c5960e44549389b87ff12886dbe2a9a
JiWon Lee <jiwon.swdev@gmail.com>
2022-04-24 22:09:02 +0900

    configure FinnanceHome StackView

--- MiniSuperApp/FinanceHome/FinanceHomeViewController.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeViewController.swift
@@ -2,42 +2,49 @@ import ModernRIBs
 import UIKit
 
 protocol FinanceHomePresentableListener: AnyObject {
-  // TODO: Declare properties and methods that the view controller can invoke to perform
-  // business logic, such as signIn(). This protocol is implemented by the corresponding
-  // interactor class.
+    // TODO: Declare properties and methods that the view controller can invoke to perform
+    // business logic, such as signIn(). This protocol is implemented by the corresponding
+    // interactor class.
 }
 
 final class FinanceHomeViewController: UIViewController, FinanceHomePresentable, FinanceHomeViewControllable {
-  
-  weak var listener: FinanceHomePresentableListener?
-  
-  init() {
-    super.init(nibName: nil, bundle: nil)
-    
-    setupViews()
-  }
-  
-  required init?(coder: NSCoder) {
-    super.init(coder: coder)
-    
-    setupViews()
-  }
-  
-  private let label: UILabel = {
-    let label = UILabel()
-    label.translatesAutoresizingMaskIntoConstraints = false
-    return label
-  }()
-  
-  func setupViews() {
-    title = "슈퍼페이"
-    tabBarItem = UITabBarItem(title: "슈퍼페이", image: UIImage(systemName: "creditcard"), selectedImage: UIImage(systemName: "creditcard.fill"))
-    label.text = "Finance Home"
-    view.backgroundColor = .systemBlue
-    view.addSubview(label)
-    NSLayoutConstraint.activate([
-      label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-      label.centerYAnchor.constraint(equalTo: view.centerYAnchor)
-    ])
-  }
+
+    weak var listener: FinanceHomePresentableListener?
+
+    // MARK: - Views
+
+    private let stackView: UIStackView = {
+        let stackView: UIStackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 4
+        return stackView
+    }()
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+
+        setupViews()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        setupViews()
+    }
+
+    func setupViews() {
+        title = "슈퍼페이"
+        tabBarItem = UITabBarItem(title: "슈퍼페이", image: UIImage(systemName: "creditcard"), selectedImage: UIImage(systemName: "creditcard.fill"))
+        view.backgroundColor = .systemBlue
+        view.addSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
 }
