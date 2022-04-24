b1ec16404b1bc1cba410e4a1d45cb1b1238029d3
JiWon Lee <jiwon.swdev@gmail.com>
2022-04-24 22:12:43 +0900

    add SuperPayDasyboardBuilder

--- MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
+++ MiniSuperApp/FinanceHome/FinanceHomeRouter.swift
@@ -1,19 +1,25 @@
 import ModernRIBs
 
 protocol FinanceHomeInteractable: Interactable {
-  var router: FinanceHomeRouting? { get set }
-  var listener: FinanceHomeListener? { get set }
+    var router: FinanceHomeRouting? { get set }
+    var listener: FinanceHomeListener? { get set }
 }
 
 protocol FinanceHomeViewControllable: ViewControllable {
-  // TODO: Declare methods the router invokes to manipulate the view hierarchy.
+    // TODO: Declare methods the router invokes to manipulate the view hierarchy.
 }
 
 final class FinanceHomeRouter: ViewableRouter<FinanceHomeInteractable, FinanceHomeViewControllable>, FinanceHomeRouting {
-  
-  // TODO: Constructor inject child builder protocols to allow building children.
-  override init(interactor: FinanceHomeInteractable, viewController: FinanceHomeViewControllable) {
-    super.init(interactor: interactor, viewController: viewController)
-    interactor.router = self
-  }
+
+    private let superPayDashboardBuildable: SuperPayDashboardBuildable
+
+    init(
+        interactor: FinanceHomeInteractable,
+        viewController: FinanceHomeViewControllable,
+        superPayDasyboardBuildable: SuperPayDashboardBuildable
+    ) {
+        self.superPayDashboardBuildable = superPayDasyboardBuildable
+        super.init(interactor: interactor, viewController: viewController)
+        interactor.router = self
+    }
 }
