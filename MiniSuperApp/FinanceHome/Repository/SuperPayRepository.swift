6d555f75d5625b61631bd3e25233f1ae49c80b2c
JiWon Lee <jiwon.swdev@gmail.com>
2022-05-07 19:57:37 +0900

    SuperPay 충전 기능

--- MiniSuperApp/FinanceHome/Repository/SuperPayRepository.swift
+++ MiniSuperApp/FinanceHome/Repository/SuperPayRepository.swift
@@ -5,14 +5,28 @@
 //  Created by nine2one on 2022/05/04.
 //
 
+import Combine
 import Foundation
 
 protocol SuperPayRepository {
     var balance: ReadOnlyCurrentValuePublisher<Double> { get }
+    func topup(amount: Double, paymentMethodID: String) -> AnyPublisher<Void, Error>
 }
 
 final class SuperPayRepositoryImp: SuperPayRepository {
     var balance: ReadOnlyCurrentValuePublisher<Double> { balanceSubject }
 
     private let balanceSubject = CurrentValuePublisher<Double>(0)
+
+    func topup(amount: Double, paymentMethodID: String) -> AnyPublisher<Void, Error> {
+        return Future<Void, Error> { [weak self] promise in
+            self?.backgroundQueue.async {
+                Thread.sleep(forTimeInterval: 2)
+                promise(.success(()))
+            }
+        }
+        .eraseToAnyPublisher()
+    }
+
+    private let backgroundQueue = DispatchQueue(label: "topup.repository.queue")
 }
